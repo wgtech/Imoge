f2a6135cf5bbde0120081f12c5cb9554fed161b7
WangGi Lim <wgtech.dev@gmail.com>
2020-11-11 00:16:35 +0900

    [Common] Update versions of dependencies; OkHttp, ConstraintLayout

--- app/src/main/java/project/wgtech/imoge/explore/view/ExploreFragment.kt
+++ app/src/main/java/project/wgtech/imoge/explore/view/ExploreFragment.kt
@@ -2,7 +2,6 @@ package project.wgtech.imoge.explore.view
 
 import android.graphics.Rect
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
